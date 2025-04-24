9313f45be917eb1d1e79268ac6a83b5c9f87a9b5
arkai <arkai@Arkaitz>
2025-04-24 17:27:23 +0200

    Tercer cambio

--- proarkaitzhernandezarkaitzlizanzu/src/proarkaitzhernandezarkaitzlizanzu/App.java
+++ proarkaitzhernandezarkaitzlizanzu/src/proarkaitzhernandezarkaitzlizanzu/App.java
@@ -6,6 +6,7 @@ public class App {
 		// TODO Auto-generated method stub
            System.out.println ("Bases de datos");
            System.out.println("Entornos");
+           System.out.println("Sistemas");
 	}
 
 }
