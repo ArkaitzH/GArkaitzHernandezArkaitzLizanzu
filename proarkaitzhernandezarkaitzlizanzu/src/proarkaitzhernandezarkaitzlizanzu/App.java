13d7add989868283170281b3ce5011a9d654c7e0
arkai <arkai@Arkaitz>
2025-04-24 18:35:01 +0200

    14

--- proarkaitzhernandezarkaitzlizanzu/src/proarkaitzhernandezarkaitzlizanzu/App.java
+++ proarkaitzhernandezarkaitzlizanzu/src/proarkaitzhernandezarkaitzlizanzu/App.java
@@ -8,7 +8,7 @@ public class App {
            System.out.println("Entornos");
            System.out.println("Sistemas");
            System.out.println("Lenguaje de marcas");
-           System.out.println("Programacion Java");
+           System.out.println("Programacion Jav");
 	}
 
 }
