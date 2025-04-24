fcb983cd10b0063109e73827012f7d35d7a0bc83
arkai <arkai@Arkaitz>
2025-04-24 16:56:31 +0200

    Inicio de aplicación

---
+++ proarkaitzhernandezarkaitzlizanzu/src/proarkaitzhernandezarkaitzlizanzu/App.java
@@ -0,0 +1,10 @@
+package proarkaitzhernandezarkaitzlizanzu;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println ("Bases de datos");
+	}
+
+}
