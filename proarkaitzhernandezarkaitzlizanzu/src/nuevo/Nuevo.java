b4abbce33a53d495a021d9d04ade1da691277ec6
arkai <arkai@Arkaitz>
2025-04-24 19:16:27 +0200

    Cambio en rama nueva 2

--- proarkaitzhernandezarkaitzlizanzu/src/nuevo/Nuevo.java
+++ proarkaitzhernandezarkaitzlizanzu/src/nuevo/Nuevo.java
@@ -5,6 +5,7 @@ public class Nuevo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		   System.out.println("Historia España");
+		   System.out.println("Historia Francia");
 	}
 
 }
